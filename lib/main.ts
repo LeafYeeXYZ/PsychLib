83bcb3db66cae56fc54b2c69ef5312c08e7bde11
LeafYeeXYZ <leafyeexyz@outlook.com>
2024-10-26 19:33:07 +0800

    ci: add github action

--- lib/main.ts
+++ lib/main.ts
@@ -1,2 +1,19 @@
+/**
+ * @module
+ * 
+ * TypeScript library that provides APIs for data analysis, especially for psychological and educational research.
+ * 
+ * @example
+ * 
+ * ```typescript
+ * import { LinearRegressionOne } from 'jsr:@leaf/psych-lib'
+ * 
+ * const x = [1, 2, 3, 4, 5]
+ * const y = [2, 4, 7, 12, 20]
+ * const regression = new LinearRegressionOne(x, y)
+ * console.log(regression.b0, regression.b1)
+ * ```
+ */
+
 export * from './base/index.ts'
 export * from './regression/index.ts'
