74372a3a2423bd812e2b9dc98a6564b3507272cd
LeafYeeXYZ <leafyeexyz@outlook.com>
2024-11-06 10:19:04 +0800

    feat: export post hoc result types

--- lib/anova/one.ts
+++ lib/anova/one.ts
@@ -269,26 +269,116 @@ export class OneWayAnova {
   }
 }
 
-type BonferroniResult = {
+/**
+ * ANOVA Post Hoc Test Result (Bonferroni)
+ * 
+ * 方差分析事后检验结果 (Bonferroni)
+ */
+export type BonferroniResult = {
+  /**
+   * Group A
+   * 
+   * A组
+   */
   groupA: string | number
+  /**
+   * Group B
+   * 
+   * B组
+   */
   groupB: string | number
+  /**
+   * Difference of mean
+   * 
+   * 均值差异
+   */
   diff: number
+  /**
+   * t value
+   * 
+   * t值
+   */
   t: number
+  /**
+   * p value
+   * 
+   * p值
+   */
   p: number
 }
 
-type ScheffeResult = {
+/**
+ * ANOVA Post Hoc Test Result (Scheffe)
+ * 
+ * 方差分析事后检验结果 (Scheffe)
+ */
+export type ScheffeResult = {
+  /**
+   * Group A
+   * 
+   * A组
+   */
   groupA: string | number
+  /**
+   * Group B
+   * 
+   * B组
+   */
   groupB: string | number
+  /**
+   * Difference of mean
+   * 
+   * 均值差异
+   */
   diff: number
+  /**
+   * F value
+   * 
+   * F值
+   */
   f: number
+  /**
+   * p value
+   * 
+   * p值
+   */
   p: number
 }
 
-type TukeyResult = {
+/**
+ * ANOVA Post Hoc Test Result (Tukey)
+ * 
+ * 方差分析事后检验结果 (Tukey)
+ */
+export type TukeyResult = {
+  /**
+   * Group A
+   * 
+   * A组
+   */
   groupA: string | number
+  /**
+   * Group B
+   * 
+   * B组
+   */
   groupB: string | number
+  /**
+   * Difference of mean
+   * 
+   * 均值差异
+   */
   diff: number
+  /**
+   * q value
+   * 
+   * q值
+   */
   q: number
+  /**
+   * p value
+   * 
+   * p值
+   */
   p: number
 }
